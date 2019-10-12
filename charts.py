ce12c7dea35fdf9d3170cba0b15fc41e027b0468
shivanshcoool <56356475+shivanshcoool@users.noreply.github.com>
2019-10-12 12:01:30 +0530

    Add files via upload

---
+++ charts.py
@@ -0,0 +1,14 @@
+#!usr/bin/python2
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+label  = ['r','s','g','h','t','u','o','p','z','x','b']
+a = np.array([2,3,6,4,8,65,2,33,44,45,11])
+color=['gold','blue','red','purple','pink','brown','black','green','yellow','white','orange']
+explode = (0, 0, 0, 0,0,0.1,0,0,0,0,0)
+
+plt.pie(a, explode=explode, colors=color,labels = label, autopct='%1.1f%%', shadow=True, startangle=140)
+plt.axis('equal')
+plt.show()
